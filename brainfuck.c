5747af22cb119b8df693f9b409b2b1bb031845cf
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-02-08 04:16:41 -0300

    Add the brainfuck interpreter

---
+++ brainfuck.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[]) {
+    char *memory = calloc(30000, sizeof(char));
+    int cell = 0;
+    char ptr;
+
+    if(argc == 1) {
+        printf("No file input was detected. \nTerminating...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE * file; 
+    char ch;
+
+    file = fopen(argv[1], "rb");
+
+    if(file == NULL) {
+        printf("File doesn't exist or couldn't be opened.");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(file, 0, SEEK_END);
+    long code_size = ftell(file);
+    rewind(file);
+    char *code = malloc(code_size);
+
+    for(int i = 0; i < code_size; i++) {
+        code[i] = fgetc(file);
+    }
+
+    for(int i = 0; i < code_size; i++) {
+        int ptr;
+        if(code[i] == '.') {
+            printf("%c", memory[cell]);
+        } else if (code[i] == '+') {
+            memory[cell]++;
+        } else if(code[i] == '-') {
+            memory[cell]--;
+        } else if(code[i] == '>') {
+            cell++;
+        } else if(code[i] == '<') {
+            cell--;
+        } else if(code[i] == '[') { 
+            ptr = i;
+        } else if(code[i] == ']') {
+            if(memory[cell] != 0) {
+                i = ptr;
+            }
+        }
+    }
+    fclose(file);
+    free(code);
+    free(memory);
+    return 0;
+}
